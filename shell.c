f722988f1e8a212aed6bc3610e2a78536ca2dad5
GabrielMh57 <abdesakhi15@gmail.com>
2023-08-23 16:22:13 -0700

    betty

--- shell.c
+++ shell.c
@@ -8,32 +8,32 @@
 int main(int ac, char **av, char **env)
 {
 	char *line = NULL;
-	size_t line_size =0;
+	size_t line_size = 0;
 	ssize_t read;
 	char *nwline = "$ ";
 	pid_t child_pid;
 
-	while(1)
+	while (1)
 	{
 		/* print $ sign to the terminal */
 		write(STDOUT_FILENO, nwline, 2);
-		
+
 		/* read data from the std input */
 		read = getline(&line, &line_size, stdin);
-		if(read == -1)
+		if (read == -1)
 		{
 			perror("Error in getline");
 			free(line);
 			exit(EXIT_FAILURE);
 		}
 
-		if(line[read - 1] == '\n')
-			line[read - 1]= '\0';
-		
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
 		/* creating a child process to execute the command */
 		child_pid = fork();
-		if
-	}	
+
+	}
 
 	free(line);
 }
